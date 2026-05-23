ae6163d58489373e08e13c8992b1eec2ee26f8a2
Elina <elina.splasa@gmail.com>
2026-05-23 16:53:58 -0500

    Home y test añadidos

---
+++ ProyectoG2/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AppProvider } from './context/AppContext.jsx' // IMPORTANTE
+import Home from './pages/Home.jsx' 
+import TestVocacional from './pages/TestVocacional.jsx'
+import Login from './pages/Login.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AppProvider> {/* 👈 Envolvemos aquí */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<TestVocacional />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/perfil" element={<Perfil />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
+  </StrictMode>,
+)
