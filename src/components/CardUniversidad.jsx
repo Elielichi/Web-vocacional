da9d74eca7385e7ce4b5c846f828751d4a42eaa4
Kenyi Nakandakari <kenyinakandakari@gmail.com>
2026-05-24 13:32:51 -0500

    Modifique Carduniversidades.jsx y Directorio.jsx (Terminé de adaptarlo con la nueva data de universidades,js)

--- src/components/CardUniversidad.jsx
+++ src/components/CardUniversidad.jsx
@@ -1,34 +1,56 @@
+import { useState } from "react";
+
 function CardUniversidad({ nombre, tipo, carreras }) {
+
+  const [mostrarCarreras, setMostrarCarreras] = useState(false);
+
   return (
+
     <div className="bg-white rounded-xl shadow-lg p-6 hover:scale-105 transition duration-300">
 
       <h2 className="text-2xl font-bold text-blue-700">
         {nombre}
       </h2>
 
-      <p className="mt-2 text-gray-600">
+      <p className="text-gray-600 mt-2 font-medium">
         Tipo: {tipo}
       </p>
 
-        <div className="mt-4">
+      <button
+        className="mt-5 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
+        onClick={() =>
+          setMostrarCarreras(!mostrarCarreras)
+        }>
 
-            <h3 className="font-bold text-gray-700 mb-2">
-                Carreras:
-            </h3>
+        {mostrarCarreras
+          ? "Ocultar carreras"
+          : "Ver carreras"}
 
-            <ul className="list-disc list-inside text-gray-600 space-y-1">
-                {carreras.map((carrera, index) => (
-                    <li key={index}>
-                    {carrera}
-                    </li>
-                ))}
-            </ul>
-        </div>
-
-      <button className="mt-4 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
-        Ver Malla
       </button>
 
+      {mostrarCarreras && (
+
+        <div className="mt-5 space-y-3">
+
+          {carreras.map((carrera, index) => (
+
+            <div
+              key={index}
+              className="border-b pb-2"
+            >
+
+              <p className="font-semibold text-gray-800">
+                {carrera.nombre}
+              </p>
+
+              <p className="text-sm text-gray-500">
+                Facultad de {carrera.facultad}
+              </p>
+
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/Directorio.jsx
+++ src/pages/Directorio.jsx
@@ -4,28 +4,22 @@ import universidades from "../data/universidades";
 function Directorio() {
 
   return (
+    <div className="min-h-screen bg-gray-100 p-10">
 
-    <div className="p-10">
-
-      <h1 className="text-4xl font-bold mb-6">
-        Universidades
+      <h1 className="text-4xl font-bold text-center mb-10 text-blue-700">
+        Directorio de Universidades
       </h1>
 
-      <div className="grid md:grid-cols-3 gap-6">
-
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 items-start">
         {universidades.map((uni, index) => (
-
           <CardUniversidad
             key={index}
             nombre={uni.nombre}
             tipo={uni.tipo}
             carreras={uni.carreras}
           />
-
         ))}
-
       </div>
-
     </div>
   );
 }
